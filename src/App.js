1f8d3c83f51f550ff249b1da9022bddb9f614c30
ali27-dev <khanali3229@gmail.com>
2025-11-18 22:06:24 +0500

    Adding the project component structure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>Logo</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h2>Form</h2>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="">
+      <h3>List</h3>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>Your packing item X is </em>
+    </footer>
   );
 }

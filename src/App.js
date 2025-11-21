255818a623072013532f26546ac1980232d7a878
ali27-dev <khanali3229@gmail.com>
2025-11-21 22:13:39 +0500

    How to control the form elements,I'm learn this in this implementation.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,21 +21,45 @@ function Logo() {
 }
 
 function Form() {
+  let [description, setDescription] = useState("");
+  let [quantity, setQunatity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newArr = {
+      description: { description },
+      quantity: { quantity },
+      packed: true,
+      id: Date(),
+    };
+    console.log(newArr);
+
+    setDescription("");
+    setQunatity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <p>What do you need for your 😍 trip?</p>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQunatity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." />
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>ADD</button>
     </form>
   );
